2da2534ba947d5a848ad7badcd6a756c6a0a719e
Gaurav Dubey <gaurav.dubey78@outlook.com>
2019-07-20 12:55:02 +0530

    Intial Commit

---
+++ src/test/java/simpleMavenProj/LoginTest.java
@@ -0,0 +1,40 @@
+package simpleMavenProj;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver","F:\\Testing\\TestingSW_And_Jars\\drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("gauravgattu@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Gattu&*D>");
+		
+	}
+	
+	
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	
+	}
+	
+}
